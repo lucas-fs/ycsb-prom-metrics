0b101c91c3f475c43ff61964bc432ae36078a08c
Lucas Ferreira da Silva <lferreira@inf.ufsm.br>
2022-05-17 03:01:47 -0300

    Transform queries to uppercase

--- prom_queries.py
+++ prom_queries.py
@@ -1,36 +1,36 @@
 from constants import IRATE_RANGE
 
-q_cpu_usage = (
+Q_CPU_USAGE = (
     'clamp_min(100 - (avg by (instance)(irate(node_cpu_seconds_total{mode="idle", job=~"rpi3.*"}'
     f'[{IRATE_RANGE}])) * 100), 0)'
 )
 
-q_network_trafic_recv_query = (
+Q_NETWORK_TRAFIC_RECV_QUERY = (
     'irate(node_network_receive_bytes_total{device="eth0",job=~"rpi3.*"}'
     f'[{IRATE_RANGE}])*8'
 )
 
-q_network_trafic_trans_query = (
+Q_NETWORK_TRAFIC_TRANS_QUERY = (
     'irate(node_network_transmit_bytes_total{device="eth0",job=~"rpi3.*"}'
     f'[{IRATE_RANGE}])*8'
 )
 
-q_io_write = (
+Q_IO_WRITE = (
     'irate(node_disk_written_bytes_total{'
     'device="sda",job=~"rpi3.*"}'
     f'[{IRATE_RANGE}])')
 
 # FIXME: Replace written metric by read metric!
-q_io_read = (
+Q_IO_READ = (
     'irate(node_disk_written_bytes_total{device="sda",job=~"rpi3.*"}'
     f'[{IRATE_RANGE}])'
 )
 
-q_nodes_swap_usage = (
+Q_NODES_SWAP_USAGE = (
     '(node_memory_SwapTotal_bytes{job=~"rpi3.*"} - node_memory_SwapFree_bytes{job=~"rpi3.*"})'
 )
 
-q_nodes_ram_usage = (
+Q_NODES_RAM_USAGE = (
     'node_memory_MemTotal_bytes{job=~"rpi3.*"} - node_memory_MemFree_bytes{job=~"rpi3.*"} - '
     '(node_memory_Cached_bytes{job=~"rpi3.*"} + node_memory_Buffers_bytes{job=~"rpi3.*"})'
 )
