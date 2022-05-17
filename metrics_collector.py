edfbe36b884437b5939ddfacca80e243e9905aa4
Lucas Ferreira da Silva <lferreira@inf.ufsm.br>
2022-05-17 03:01:47 -0300

    Transform queries to uppercase

--- metrics_collector.py
+++ metrics_collector.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
         executions_logs = json.load(raw_logfile)
 
     queries = {
-        'cpu_usage': q_cpu_usage,
-        'network_recv': q_network_trafic_recv_query,
-        'network_trans': q_network_trafic_trans_query,
-        'io_written': q_io_write,
-        'io_read': q_io_read,
-        'ram_usage': q_nodes_ram_usage,
-        'swap_usage': q_nodes_swap_usage
+        'cpu_usage': Q_CPU_USAGE,
+        'network_recv': Q_NETWORK_TRAFIC_RECV_QUERY,
+        'network_trans': Q_NETWORK_TRAFIC_TRANS_QUERY,
+        'io_written': Q_IO_WRITE,
+        'io_read': Q_IO_READ,
+        'ram_usage': Q_NODES_RAM_USAGE,
+        'swap_usage': Q_NODES_SWAP_USAGE
     }
 
     metrics_data = generate_metrics_json(executions_logs, queries, "ycsb_execution_metrics.json")
