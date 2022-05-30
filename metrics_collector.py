5c3d8eb7817449aea095b6b959fbe545b20a8ed2
Lucas Ferreira da Silva <lferreira@inf.ufsm.br>
2022-05-30 02:04:58 -0300

    Add new queries to collect and export aggregated metrics datasets

--- metrics_collector.py
+++ metrics_collector.py
@@ -1,7 +1,17 @@
 import json
+from os.path import exists
+from os import makedirs
 
-from metrics import generate_metrics_json
+from tqdm import tqdm
+
 from prom_queries import *
+from metrics import (
+    generate_metrics_json,
+    aggregate_metric_by_nodes,
+    export_aggregated_metrics_csv
+)
+
+DATASETS_PATH = "metrics_datasets/"
 
 if __name__ == "__main__":
 
@@ -10,15 +20,24 @@ if __name__ == "__main__":
         executions_logs = json.load(raw_logfile)
 
     queries = {
-        'cpu_usage': Q_CPU_USAGE,
-        'network_recv': Q_NETWORK_TRAFIC_RECV_QUERY,
-        'network_trans': Q_NETWORK_TRAFIC_TRANS_QUERY,
-        'io_written': Q_IO_WRITE,
-        'io_read': Q_IO_READ,
-        'ram_usage': Q_NODES_RAM_USAGE,
-        'swap_usage': Q_NODES_SWAP_USAGE
+        "cpu_usage_percentage": Q_NODES_CPU_USAGE_PERCENTAGE,
+        "cpu_iowait_percentage": Q_NODES_CPU_IOWAIT_PERCENTAGE,
+        "network_recv_bits": Q_NODES_NETWORK_TRAFIC_RECV,
+        "network_trans_bits": Q_NODES_NETWORK_TRAFIC_TRANS,
+        "io_sda_written_bytes": Q_NODES_IO_WRITE,
+        "io_sda_readed_bytes": Q_NODES_IO_READ,
+        "ram_usage_bytes": Q_NODES_RAM_USAGE,
+        "swap_usage_bytes": Q_NODES_SWAP_USAGE,
+        "usb_storage_used_space_bytes": Q_NODES_STORAGE_USED_SPACE
     }
 
     metrics_data = generate_metrics_json(executions_logs, queries, "ycsb_execution_metrics.json")
 
-    print("DONE!")
+    if not exists(DATASETS_PATH):
+        makedirs(DATASETS_PATH)
+
+    for query_name in tqdm(queries.keys(), desc="Generating metrics datasets"):
+        aggregated_metric = aggregate_metric_by_nodes(metrics_data, query_name)
+        export_aggregated_metrics_csv(aggregated_metric, f"{DATASETS_PATH}{query_name}.csv")
+
+    print("FINISHED!")
